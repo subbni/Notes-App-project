f4424deba52e0339f88d16eee3bd742e1f4db492
subbni <slfkak@naver.com>
2024-08-08 13:25:44 +0900

    [feat] add notes click action
    - notes 중 하나 클릭 시 포커스 되는 효과 구현

--- components/Notes.js
+++ components/Notes.js
@@ -1,6 +1,8 @@
 import { getNotes } from '../services/dataService.js';
 import { showNoteById } from './Note.js';
 
+let previousClickedElement = null;
+
 export function initializeNotes() {
   // 첫 로드시 모든 노트를 보여줌
   const notes = getNotes('All');
@@ -24,8 +26,16 @@ export function getNotesByDirectory(directory) {
 }
 
 function handleNotesClick(e) {
-  const noteId = e.target.closest('.notes-item').dataset.id;
-  // e.target.dataset.id 가 아닌 이유는?
+  const clickedElement = e.target.closest('.notes-item');
+
+  if (previousClickedElement) {
+    previousClickedElement.classList.remove('clicked');
+  }
+
+  clickedElement.classList.add('clicked');
+  previousClickedElement = clickedElement;
+
+  const noteId = clickedElement.dataset.id;
   showNoteById(noteId);
 }
 
@@ -33,7 +43,8 @@ function createNoteItemElement(note) {
   return `
     <div class="notes-item" data-id=${note.id}>
       <h2 class="notes-item__title">${note.title}</h2>
-      <p class="notes-item__directory">/${note.directory}</p>
+      <img src="./images/folder.svg" alt="folder icon">
+      <span class="notes-item__directory">/${note.directory}</span>
     </div>
   `;
 }
